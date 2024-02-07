358dce63ae4837dc7e8a4270df9577d77165e431
Ccdwe <dweckspam@gmail.com>
2024-02-07 12:49:46 +0200

    moved app.py to the root

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ if 'DYNO' in os.environ:
 
 # import the create app function 
 # that lives in src/__init__.py
-from src import create_app
-from Model.League import League
-from Model.RoundOfAuction import RoundOfAuction
-from Model.Team import Team
+from flask_app.src import create_app
+from flask_app.Model.League import League
+from flask_app.Model.RoundOfAuction import RoundOfAuction
+from flask_app.Model.Team import Team
 from flask import jsonify, current_app
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
